5431867b884b385a0d76a8102c7b635660f452ab
raik.tatyana <raik.tatyana@gmail.com>
2019-03-04 22:48:30 +0300

    task 15

--- Addressbook-web-tests/Addressbook-web-tests/Tests/ContactCreationTest.cs
+++ Addressbook-web-tests/Addressbook-web-tests/Tests/ContactCreationTest.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
+using System.Xml;
 using System.Xml.Serialization;
 using Newtonsoft.Json;
 using NUnit.Framework;
@@ -37,13 +38,13 @@ namespace WebAddressbookTests
                 });
             }
             return contacts;
-
         }
 
         public static IEnumerable<ContactData> ContactDataFromXmlFile()
         {
             return (List<ContactData>)
-                 new XmlSerializer(typeof(List<ContactData>)).Deserialize(new StreamReader(@"contacts.xml"));
+                 new XmlSerializer(typeof(List<ContactData>))
+                 .Deserialize(new StreamReader(@"contacts.xml"));
         }
 
         public static IEnumerable<ContactData> ContactDataFromJsonFile()
@@ -52,7 +53,7 @@ namespace WebAddressbookTests
                     File.ReadAllText(@"contacts.json"));
         }
 
-        [Test, TestCaseSource("ContactDataFromXmlFile")]
+        [Test, TestCaseSource("ContactDataFromJsonFile")]
         public void CreateContactTest(ContactData contact)
         {
             List<ContactData> oldContacts = app.Contacts.GetContactList();
