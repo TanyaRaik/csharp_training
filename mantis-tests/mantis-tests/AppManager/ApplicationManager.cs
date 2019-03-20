370364a16dc68a5c424baff4360d59fd4da4a26b
raik.tatyana <raik.tatyana@gmail.com>
2019-03-20 20:16:39 +0300

    9.1 лекция

---
+++ mantis-tests/mantis-tests/AppManager/ApplicationManager.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace mantis_tests
+{
+    public class ApplicationManager
+    {
+        protected IWebDriver driver;
+        protected string baseURL;
+        private int attempt;
+
+        public RegistrationHelper Registration { get; set; }
+
+        public FtpHelper Ftp { get; set; }
+
+        private static ThreadLocal<ApplicationManager> app = new ThreadLocal<ApplicationManager>();
+
+        private ApplicationManager()
+        {
+            driver = new FirefoxDriver();
+            baseURL = "http://localhost";
+            attempt = 0;
+            Registration = new RegistrationHelper(this);
+            Ftp = new FtpHelper(this);            
+        }
+
+        ~ApplicationManager()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+        }
+
+        public static ApplicationManager GetInstatnce()
+        {
+            if (! app.IsValueCreated)
+            {
+                ApplicationManager newInstance = new ApplicationManager();
+                newInstance.driver.Url = "http://localhost/mantisbt-2.20.0/login_page.php";
+                app.Value = newInstance;
+            }
+            return app.Value;
+        }
+
+        public int Attempt
+        {
+            get
+            {
+                return attempt;
+            }
+        }
+
+        public IWebDriver Driver
+        {
+            get
+            {
+                return driver;
+            }
+        }
+    }
+}
